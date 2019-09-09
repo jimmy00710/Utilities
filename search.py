33988e331f23b28ba21212d86e3a54573219d5a4
bitroid <jim00710@yahoo.com>
2019-09-09 11:55:18 +0530

    Added comments in the code

--- search.py
+++ search.py
@@ -1,10 +1,22 @@
 import os
 from tqdm import tqdm_notebook
 
+'''
+Search function search file recursively
 
+Input Argument - 
+base path - base path of directory in which search is to be done.
+extension - extension of file which has to be search. 
+
+Output-
+return list of files from in that directory.
+
+
+This method is the fastest way of recursive file search in python
+'''
 def get_files_generator(base_path,file_extension):
     for entry in os.scandir(base_path):
-        if entry.is_file() and entry.name.endswith(file_extension):
+        if entry.is_file() and entry.name.endswith(file_extension): #In this line more file extension condition can be added. 
             yield entry.path
         elif entry.is_dir():
             yield from get_files_generator(entry.path)
